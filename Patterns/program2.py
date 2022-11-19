f7a6723c101b35bb5020101733e6506d4d8a9083
Prateek Mohan <prateeksingh3290@gmail.com>
2022-11-19 11:56:44 +0530

    .

--- Patterns/program2.py
+++ Patterns/program2.py
@@ -2,8 +2,34 @@
 2222
 3333
 4444
-n=int(input(":"))
+
+'''n=int(input(":"))
 for i in range(1,n+1):
     for j in range(1,5):
         print(i,end="")
-    print()
+    print()'''
+
+
+1234
+1234
+1234
+1234
+
+'''n=int(input(":"))
+for i in range(1,n+1):
+    for j in range(1,5):
+        print(j,end="")
+    print()'''
+
+
+4321
+4321
+4321
+4321
+'''n=int(input(":"))
+for i in range(1,n+1):
+    x = n
+    for j in range(1,5):
+        print(x,end="")
+        x=x-1
+    print()'''
