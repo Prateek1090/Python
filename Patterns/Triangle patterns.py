c235783f33248a19004a639eda92f0fdb956373c
Prateek Mohan <prateeksingh3290@gmail.com>
2022-11-20 09:18:43 +0530

    some changes were made

--- Patterns/Triangle patterns.py
+++ Patterns/Triangle patterns.py
@@ -36,7 +36,8 @@ for i in range(1,n+1):
         print(x,end="")
         x=x+1
     print()
-    
+
+
 1
 2 1
 3 2 1
@@ -48,7 +49,8 @@ for i in range(1,n+1):
         print(i,end=" ")
         i=i-1
     print()
-    
+
+
 5 
 4 5 
 3 4 5 
