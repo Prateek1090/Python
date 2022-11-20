20edf61943ca74cb9c9e46da698056bfbbb1e1be
Prateek Mohan <65453254+Prateek1090@users.noreply.github.com>
2022-11-20 08:30:41 +0530

    Update Triangle patterns.py

--- Patterns/Triangle patterns.py
+++ Patterns/Triangle patterns.py
@@ -35,3 +35,32 @@ for i in range(1,n+1):
         print(x,end="")
         x=x+1
     print()
+    
+1
+2 1
+3 2 1
+4 3 2 1
+
+n=int(input(":"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+        i=i-1
+    print()
+    
+5 
+4 5 
+3 4 5 
+2 3 4 5 
+1 2 3 4 5 
+
+n=int(input(":"))
+x=0
+for i in range(1,n+1):
+    y=1
+    for j in range(1,i+1):
+        z=n-x
+        print(z+y-1,end=" ")
+        y+=1
+    print()
+    x+=1    
