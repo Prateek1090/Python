24ca62dbbb49e24213f332109fb421d1c2334ba3
Prateek Mohan <prateeksingh3290@gmail.com>
2022-11-20 08:43:59 +0530

    some changes were made

--- Patterns/Alphabet patterns.py
+++ Patterns/Alphabet patterns.py
@@ -10,9 +10,8 @@ for i in range(1,n+1):
         x=chr(ord('A')+j-1)
         print(x,end=" ")
     print()
-'''
 
-'''
+
 A B C D 
 B C D E 
 C D E F 
@@ -26,9 +25,8 @@ for i in range(1,n+1):
         print(x,end=" ")
         m=m+1
     print()
-'''
 
-'''
+
 A 
 B C 
 C D E 
@@ -42,10 +40,8 @@ for i in range(1,n+1):
         print(x,end=" ")
         m=m+1
     print()
-'''
 
 
-'''
 E 
 D E 
 C D E 
@@ -62,4 +58,51 @@ for i in range(1,n+1):
         a=a+1
     print()
     z=z+1
+
+
+A 
+A B 
+A B C 
+A B C D
+
+n=int(input(":"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        ch=chr(ord("A")+j-1)
+        print(ch,end=" ")
+        j+=1
+    print()
+    i+=1
+
+
+A B C D 
+A B C 
+A B 
+A
+
+n=int(input(":"))
+x=n
+for i in range(1,n+1):
+    for j in range(1,x+1):
+        ch=chr(ord("A")+j-1)
+        print(ch,end=" ")
+        j+=1
+    print()
+    x-=1
+
+
+
+A
+B B
+C C C
+D D D D
+
+n=int(input(":"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        ch=chr(ord("A")+i-1)
+        print(ch,end=" ")
+        j+=1
+    print()
+    i+=1
 '''
